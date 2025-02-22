6a86cca78c7e5735d7066af21845e6e33f3edc91
roy <7907969+ohroy@users.noreply.github.com>
2025-02-22 13:39:01 +0800

    add lmo command

--- commands/listen.py
+++ commands/listen.py
@@ -1,32 +1,46 @@
 import lldb
 import fbchisellldbbase as fb
 from typing import cast
-from typing_extensions import override
 import threading
 
 debugger = cast(lldb.SBDebugger, lldb.debugger)
-
 
 
 def lldbcommands():
     return [ListenModuleWithOffset()]
 
 
+def set_breakpoint(target: lldb.SBTarget, module: lldb.SBModule, offset: int):
+    """在指定模块 + 偏移处设置断点"""
+    file_header_addr = module.GetObjectFileHeaderAddress()  # type: lldb.SBAddress
+    base_addr = file_header_addr.GetLoadAddress(target)
+    if base_addr == lldb.LLDB_INVALID_ADDRESS:
+        print(f"无法获取模块基地址")
+        return
+
+    breakpoint_addr = base_addr + offset
+    print(f"在 {target} 的 {hex(breakpoint_addr)} 处设置断点")
+
+    bp = target.BreakpointCreateByAddress(breakpoint_addr)
+    if bp.IsValid():
+        print(f"断点设置成功: {bp}")
+    else:
+        print(f"断点设置失败")
+
+
 class ListenModuleWithOffset(fb.FBCommand):
-    @override
     def name(self) -> str:  # type: ignore
         return "lmo"
 
-    @override
     def description(self) -> str:  # type: ignore
         return "listen module load and set breakpoint with offset"
 
     def args(self):
         return [
             fb.FBCommandArgument(
-                arg="viewOrLayer",
-                type="UIView/NSView/CALayer *",
-                help="The view/layer to border. NSViews must be layer-backed.",
+                arg="module",
+                type="str",
+                help="the module name",
             ),
             fb.FBCommandArgument(
                 arg="offset",
@@ -57,15 +71,18 @@ class ListenModuleWithOffset(fb.FBCommand):
                     ):
                         if traceOn:
                             num_modules = lldb.SBTarget.GetNumModulesFromEvent(event)
-                            for i in range(num_modules):
-                                module = lldb.SBTarget.GetModuleAtIndexFromEvent(
-                                    event, i
+                            module: lldb.SBModule = (
+                                lldb.SBTarget.GetModuleAtIndexFromEvent(
+                                    num_modules - 1, event
                                 )
-                                print(f"加载模块: {module.GetFileSpec()}")
-                            # print('Event description:', desc)
-                            # print('Event data flavor:', event.GetDataFlavor())
-                            # print('Process state:', lldbutil.state_type_to_str(process.GetState()))
-                            # print()
+                            )  # type: lldb.SBModule
+                            spec: lldb.SBFileSpec = module.GetFileSpec()
+                            filename: str = spec.GetFilename()
+                            print(f"加载模块: {module.GetFileSpec()}")
+                            if filename == arguments[0]:
+                                print(f"目标模块已加载，设置断点...")
+                                set_breakpoint(target, module, int(arguments[1], 16))
+                                break
                     else:
                         if traceOn:
                             print("timeout occurred waiting for event...")
@@ -74,4 +91,4 @@ class ListenModuleWithOffset(fb.FBCommand):
 
         my_thread = MyListeningThread()
         my_thread.start()
-        my_thread.join()
+        # my_thread.join()
